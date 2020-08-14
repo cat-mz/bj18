f7bf8336aa0bdf841e9cb75d631e2cd59d9d80fc
cat <cat@2297533945.com>
2020-08-14 11:12:42 +0800

    创建login

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('ojbk')
 
+def login(request):
+    return redirect('/index')
